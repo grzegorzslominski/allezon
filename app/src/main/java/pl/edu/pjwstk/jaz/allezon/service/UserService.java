25c8ab44815cbb1c694582db3461e685adf705b9
Grzegorz Slominski <grzegorzslominski@gmail.com>
2021-01-26 18:51:42 +0100

    add UserService to git track

---
+++ app/src/main/java/pl/edu/pjwstk/jaz/allezon/service/UserService.java
@@ -0,0 +1,47 @@
+package pl.edu.pjwstk.jaz.allezon.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.edu.pjwstk.jaz.allezon.entity.UserEntity;
+import pl.edu.pjwstk.jaz.allezon.repository.UserRepository;
+import pl.edu.pjwstk.jaz.allezon.security.AuthenticationToken;
+import pl.edu.pjwstk.jaz.allezon.security.UserSession;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+private final UserRepository userRepository;
+private final PasswordEncoder passwordEncoder;
+private final UserSession userSession;
+private final EntityManager entityManager;
+
+
+    public void saveUser(UserEntity user) {
+        user.setEmail(user.getEmail());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole_id(2);
+        userRepository.save(user);
+    }
+
+    public boolean login(UserEntity user) {
+        Optional  userEntity =  userRepository.findByEmail(user.getEmail());
+        if (userEntity.isPresent()) {
+            UserEntity userEntity1 = (UserEntity) userEntity.get();
+            if (passwordEncoder.matches(user.getPassword(), userEntity1.getPassword())) {
+                userSession.logIn();
+                SecurityContextHolder.getContext().setAuthentication(new AuthenticationToken(userEntity1, entityManager));
+                return true;
+            }
+        }
+        return false;
+    }
+}
